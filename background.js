09612a9b005ff4d39ad057c9f52aa2df28d83b7f
Amine <amine1u1@outlook.com>
2020-11-24 00:35:08 +0100

    Add a button to get NSFW categorization results from a ML web service (Imagga)

--- background.js
+++ background.js
@@ -1,4 +1,47 @@
+DEFAULT_API_KEY = 'acc_d706df0b98e201b';
+DEFAULT_API_SECRET = '4c01b561518ffeba42c11d0041a3bbdf';
 
 chrome.webRequest.onCompleted.addListener((info) => {
     chrome.tabs.sendMessage(info.tabId, {command: "processReceivedMedias"})
 }, {urls: ["*://ton.twitter.com/*/data/dm/*",/*"*://pbs.twimg.com/dm_*_preview/*",*/"*://pbs.twimg.com/*"], types: ["image"]})
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    switch (message.command) {
+        case "categorize":
+            chrome.storage.sync.get(
+                {
+                    "apiKey": DEFAULT_API_KEY,
+                    "apiSecret": DEFAULT_API_SECRET
+                },
+                ({apiKey, apiSecret}) => {
+                    getImaggaCategorization(message.imageBlobUrl, apiKey, apiSecret)
+                        .then(r => sendResponse(r));
+                }
+            )
+            return true;//We return true to inform the browser that the response will be sent asynchronously
+    }
+});
+
+
+async function getImaggaCategorization(imageBlobUrl, apiKey, apiSecret) {
+
+    const formData = new FormData();
+    let imageBlob = await fetch(imageBlobUrl).then(r => r.blob());
+
+    formData.append('image', imageBlob);
+
+    return fetch("https://api.imagga.com/v2/categories/nsfw_beta", {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+            'Authorization': 'Basic ' + btoa(`${apiKey}:${apiSecret}`)
+        },
+        body: formData
+    })
+        .then(response => response.json())
+        .then(response => {
+            if (response.status.type === "success")
+                return response.result.categories
+        })
+        .catch(error => console.log('Error:', error));
+}
--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,8 @@
 unprocessedReceivedMediasSelector = ".css-1dbjc4n.r-1habvwh.r-13awgt0:not(.saferTwitter-processed)";
 imageContainerSelector = ".css-1dbjc4n.r-1p0dtai.r-1mlwlqe.r-1d2f490.r-1udh08x.r-u8s1d.r-zchlnj.r-ipm5af.r-417010";
+categorizableImageContainerSelector = ".css-1dbjc4n.r-1p0dtai.r-1mlwlqe.r-1d2f490.r-1udh08x.r-u8s1d.r-zchlnj.r-ipm5af.r-417010:not(.r-sdzlij):not(.r-1niwhzg):not([data-testid=tweetPhoto])";
+
+confidenceThreshold = 10;
 
 
 chrome.runtime.onMessage.addListener(message => {
@@ -33,6 +36,77 @@ function InjectButtonsNextToMedia(elem) {
     });
     buttonsContainer.append(button_unblur);
 
+    if (elem.querySelector(categorizableImageContainerSelector)) {
+        var button_ML = document.createElement("button");
+        button_ML.classList.add("saferTwitter-button");
+        button_ML.classList.add("saferTwitter-ML-button");
+        button_ML.appendChild(document.createTextNode("Calculate image safety"));
+        button_ML.addEventListener('click', () => {
+            var imageContainer = elem.querySelector(categorizableImageContainerSelector);
+            button_ML.disabled = true;
+            if (imageContainer) {
+                button_ML.textContent = "Processing"
+                ProcessImageCategorization(button_ML, imageContainer.lastElementChild.src);
+            } else
+                button_ML.textContent = "Media not supported";
+        }, {once: true});
+        buttonsContainer.append(button_ML);
+    }
     elem.classList.add("saferTwitter-processed");
     buttonsContainer.parentElement.classList.add("saferTwitter-buttonsContainer");
 }
+
+
+function ExtractTwitterImgUrlFromThumbnail(thumbnailUrl) {
+    var imageUrl = new URL(thumbnailUrl);
+    var idx = imageUrl.pathname.indexOf(":");
+    if (idx !== -1)
+        imageUrl.pathname = imageUrl.pathname.substring(0, idx);
+    return imageUrl.href;
+}
+
+function DownloadTwitterImg(twitterImgUrl) {
+    return fetch(twitterImgUrl, {
+        "headers": {
+            "accept": "image/webp,image/apng,image/*,*/*;q=0.8"
+        },
+        "referrer": "https://twitter.com/",
+        "body": null,
+        "method": "GET",
+        "credentials": "include"
+    })
+}
+
+
+function GetCategorizationResultsFromAPI(twitterImgUrl, callback) {
+    DownloadTwitterImg(twitterImgUrl)
+        .then(response => response.blob())
+        .then(imageBlob =>
+            chrome.runtime.sendMessage({
+                command: "categorize",
+                imageBlobUrl: URL.createObjectURL(imageBlob)
+            }, callback)
+        );
+}
+
+
+/**
+ * Function to get image categorization results and write them to a specified DOM Element.
+ * @param resultElement Node where the results will be added as textContent
+ * @param thumbnailUrl url of the displayed twitter thumbnail
+ */
+function ProcessImageCategorization(resultElement, thumbnailUrl) {
+    GetCategorizationResultsFromAPI(ExtractTwitterImgUrlFromThumbnail(thumbnailUrl), (categorizationResults) => {
+        var text = "";
+        if (categorizationResults)
+            categorizationResults.forEach(({confidence, name}) => {
+                if (confidence > confidenceThreshold)
+                    text += name.en + confidence.toPrecision(3) + "% " + "\r\n";
+            })
+        else
+            text = "Processing Error";
+        resultElement.textContent = text;
+    });
+}
+
+
